bcb907d672f5a3961b0f515c23187016d887ba62
Recky <remyashimirwe48@gmail.com>
2026-02-09 10:54:27 +0200

    explaining highest

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const score=[85,92,78,90,88]
+const scores = [85,92,78,90,88]
 const output = document.getElementById("output")
-text = "<ol>"
-for(let i=0; i< score.length; i++){
-    text += "<li>" +score[i] + "</li>"
+const high = document.getElementById("highest")
+// first method
+text = "<ul>"
+for(let i=0; i< scores.length; i++){
+    text += "<li>" +scores[i] + "</li>"
 }
-text = "</ol>"
+text += "</ul>"
 output.innerHTML = text
+//second method
+output.innerHTML += "<h3>Student marks</h3>"
+for(let i = 0; i < scores.length; i++  ){
+    output.innerHTML += scores[i] + "<br>"
+}
+//highest score
+let highest = scores[0]
+for(let i= 0; i< scores.length; i++){
+    if(scores[i] > highest){
+        highest = scores[i]
+    }
+}
+high.innerHTML = highest
+console.log(highest)
+
+//lowerest score
 
 
 
