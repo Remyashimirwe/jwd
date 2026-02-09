8cf94aa2e24446171db7e3f4652b819f1963989e
toussaintmugisha1 <toussaintmugisha1@gmail.com>
2026-02-09 10:24:15 +0200

    toussa

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-const cart = ["laptop", "Mouse", "Keyboard", "Monitor"];
-const output = document.getElementById("cart");
-//add the element 
-cart.push("Headphones");
-//display the the cart
-output.innerHTML += "<h2>Product in cart</h2>";
-for (let i=0; i <cart.length; i++){
-    output.innerHTML += cart[i] + "<br>"
+const score=[85,92,78,90,88]
+largest = 0
+for (let i= 0; i< score.length; i++) {
+    const element = array[index];
+    
 }
-// remove the element but the last one
-cart.pop()
-// lenth 
-size = cart.length
-document.getElementById("length").innerHTML += "<h3>length of the cart</h3>"
-document.getElementById("length").innerHTML += size
-// dislplay last
-document.getElementById("elem").innerHTML += "<p>this the first product</p>" 
-document.getElementById("elem").innerHTML += cart[0]
-document.getElementById("elem").innerHTML += "<p>this the last product</p>"
-document.getElementById("elem").innerHTML += cart[cart.length -1 ]
 
 
