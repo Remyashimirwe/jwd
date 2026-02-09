2dee729713e6119d158b27c0d30ddf58546ceab1
Recky <remyashimirwe48@gmail.com>
2026-02-09 10:34:54 +0200

    score analysis

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const score=[85,92,78,90,88]
-largest = 0
-for (let i= 0; i< score.length; i++) {
-    const element = array[index];
-    
+const output = document.getElementById("output")
+text = "<ol>"
+for(let i=0; i< score.length; i++){
+    text += "<li>" +score[i] + "</li>"
 }
+text = "</ol>"
+output.innerHTML = text
+
 
 
