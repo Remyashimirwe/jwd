81183514189dd5038ce906ef7452d6f320ddf334
recky <remyashimirwe48@gmail.com>
2026-02-23 10:24:00 +0200

    working with the itarator of regex

--- iteration.md
+++ iteration.md
@@ -12,3 +12,19 @@ Create a new array containing only the even numbers from:'
 ```javascript
 const mixed = [1, 2, 5, 8, 11, 14];
 ```
+## Exercises
+1. 	Match the "Javascript " word  in a string.
+2. 	Match the word "Hello"  regardless of case.
+3. 	Match the word  "cat" OR "dog" .
+4. 	Match any 3‑letter word.
+5. 	Match any character except newline.
+
+### SECTION 2 — Using match(), search(), replace(), test()
+#### Exercises
+
+6. 	Extract all occurrences of the word "code".
+
+7. 	Replace all spaces with hyphens.
+8. 	Search for the first digit in a string.
+9. 	Test if a string contains the word "admin".
+10. Replace all numbers with #.
---
+++ regular_Expressions.js
@@ -0,0 +1,42 @@
+// regular expression : //regex /rgexp
+// /pattern/modifiers
+//i 
+// literal notation
+const pattern = /hello/i
+// rgexp objext
+const patterns = new RegExp("hello", "i") 
+//using regex with string 
+/* - march()
+ - replace()
+ -search()
+ - test()
+ */
+//example 
+let text = "welcome to the code of the world !!"
+result = text.match(/welcome/.test(text))
+console.log(result)
+// example replace()
+let replace = text.replace(/code/gi , "Magic")
+console.log(replace)
+// example search()
+let search = text.search(/world/)
+console.log(search)
+// example to test()
+let text2 = "Hello world";
+let pattern1= /t/g;
+let result2 = pattern1.test(text2)
+console.log(result2)
+//ignore_case i
+let pattern2 = /code/i
+console.log(pattern2.test(text))
+//global g
+let pattern3 = /e/g
+console.log(pattern3.test(text))
+// multi_line ^ and $ works per line 
+sext3 = "world\nhello";
+let pattern4 = /$world/
+console.log(pattern4.test(sext3))
+const regex = /^Hello/;
+const text3 = "Hello world\nHello again";
+
+console.log(regex.test(text3)); 
