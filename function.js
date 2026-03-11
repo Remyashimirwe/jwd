119bedf3c2a0bc98928612ef340de3ccb2c6c3ba
Recky <remyashimirwe48@gmail.com>
2026-03-11 10:03:33 +0200

    functions

---
+++ function.js
@@ -0,0 +1,10 @@
+function greeting(){
+    return "Hello, welcome";
+}
+function add(a, b){
+    return (a + b);
+}
+a = parseInt(prompt("enter number"));
+b = parseInt(prompt("enter number b"))
+result = add(a,b);
+console.log(result);
