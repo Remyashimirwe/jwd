3af4f1bf93d18d1c08250c498cfff068cc053582
Recky <remyashimirwe48@gmail.com>
2026-03-12 11:22:52 +0200

    doing the supermarket list and object

---
+++ corr.js
@@ -0,0 +1,14 @@
+function calculateFare(distance, rate) {
+  console.log(distance * rate);
+}
+
+function applyDiscount() {
+  const price = document.getElementById("price");
+  priceInput = price.value;
+  discounts = [];
+
+  pricedata = [];
+  pricedata.push(priceInput);
+  console.log(priceInput);
+  console.log(pricedata);
+}
